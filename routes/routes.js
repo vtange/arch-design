a5b6816866c9bccdf095d210338a69b7f95a6a76
Victor Tang <victortang1992@gmail.com>
2016-02-24 00:55:16 -0800

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,28 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
+	
+    app.get('/ukiyo', function(req, res) {
+        res.render('ukiyo.ejs'); // load the index.ejs file
+    });
+	
+    app.get('/live-make', function(req, res) {
+        res.render('live-make.ejs'); // load the index.ejs file
+    });
+	
+    app.get('/rolling-thunder', function(req, res) {
+        res.render('rolling-thunder.ejs'); // load the index.ejs file
+    });
+	
+    app.get('/shrouded-pearl', function(req, res) {
+        res.render('shrouded-pearl.ejs'); // load the index.ejs file
+    });
+	
+    app.get('/sf-village', function(req, res) {
+        res.render('sf-village.ejs'); // load the index.ejs file
+    });
+	
+	app.get('/sf-resurrection', function(req, res) {
+        res.render('sf-resurrection.ejs'); // load the index.ejs file
+    });
 }
