325cd7e9ddda47a3a7cb4211f860900a55193ab1
Victor Tang <victortang1992@gmail.com>
2016-03-01 14:25:12 -0800

    use images instead of fa-icons

--- public/sf-village/assets/js/main.js
+++ public/sf-village/assets/js/main.js
@@ -243,6 +243,25 @@
 
 						}
 					});
+				$('.features section')
+					.each(function() {
+
+					var	$this = $(this),
+						$image = $this.find('.image'),
+						$img = $image.find('img'),
+						x;
+
+					// Assign image.
+						$image.css('background-image', 'url(' + $img.attr('src') + ')');
+
+					// Set background position.
+						if (x = $img.data('position'))
+							$image.css('background-position', x);
+
+					// Hide <img>.
+						$img.hide();
+
+				});
 
 	});
 
