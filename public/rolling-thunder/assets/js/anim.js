f35f3d2b63a3070baa37b82330675e93a65ff086
Victor Tang <victortang1992@gmail.com>
2016-03-03 21:23:44 -0800

    intro animation

--- public/rolling-thunder/assets/js/anim.js
+++ public/rolling-thunder/assets/js/anim.js
@@ -1,10 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+var $header = document.getElementById("header");
+
 function fadeIn(el) {
-  el.style.opacity = 1;
+ if (!el.style){
+	 el[0].style.opacity = 1;
+ }
+ else{
+ 	 el.style.opacity = 1;
+ }
 }
 
-window.setTimeout(function(){fadeIn(document.getElementById("background"))},500);
+window.setTimeout(function(){
+	fadeIn(document.getElementById("background"));
+	window.setTimeout(function(){
+		fadeIn($header.getElementsByTagName("h1"));
+		window.setTimeout(function(){
+			fadeIn($header.getElementsByTagName("p"));
+		},500);
+	},500);
+},500);
 
 
+	
+	
 });
