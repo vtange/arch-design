f8f5e692ddedcdf59902414e782b7fa17196aa35
Victor Tang <victortang1992@gmail.com>
2016-03-03 23:22:20 -0800

    credit

--- public/rolling-thunder/assets/js/main.js
+++ public/rolling-thunder/assets/js/main.js
@@ -1,6 +1,8 @@
 /*
 	Eventually by HTML5 UP
 	html5up.net | @n33co
+	Modified by vtange
+	vtange.net  | @_vtange
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
